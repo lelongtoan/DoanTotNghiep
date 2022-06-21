b9858022af665531bb981807db4cf4d1a3a0f590
lelongtoan <83998616+lelongtoan@users.noreply.github.com>
2022-06-22 02:10:36 +0700

    Revert "layout member"
    This reverts commit fb902458c4497e740708744fd560a2e590e75bbb.

---
+++ doanasp/doanasp/Migrations/20220621092829_first.cs
@@ -0,0 +1,495 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace doanasp.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Accounts_AccountId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoides_Accounts_AccountId",
+                table: "Invoides");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoides_AccountId",
+                table: "Invoides");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_AccountId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "TypeName",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Invoides");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Invoides");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Carts");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Invoides",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "InvoiceDetails",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "ProductTypes",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ProductTypes",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Createddate",
+                table: "Products",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Discount",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProducerId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Createddate",
+                table: "Invoides",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Invoides",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "Invoides",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Createddate",
+                table: "InvoiceDetails",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Discount",
+                table: "InvoiceDetails",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerId",
+                table: "Carts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(maxLength: 20, nullable: false),
+                    Password = table.Column<string>(maxLength: 20, nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Fullname = table.Column<string>(nullable: true),
+                    Createddate = table.Column<DateTime>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    Avatar = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductDetails",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId1 = table.Column<int>(nullable: true),
+                    Importprice = table.Column<int>(nullable: false),
+                    Importquantity = table.Column<int>(nullable: false),
+                    Totalprice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductDetails", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_ProductDetails_Products_ProductId1",
+                        column: x => x.ProductId1,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(maxLength: 50, nullable: false),
+                    Password = table.Column<string>(maxLength: 50, nullable: false),
+                    positionid = table.Column<int>(nullable: false),
+                    Pay = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    Fullname = table.Column<string>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Createddate = table.Column<DateTime>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    Avatar = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Positions_positionid",
+                        column: x => x.positionid,
+                        principalTable: "Positions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProducerId",
+                table: "Products",
+                column: "ProducerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoides_CustomerId",
+                table: "Invoides",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoides_EmployeeId",
+                table: "Invoides",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_CustomerId",
+                table: "Carts",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_positionid",
+                table: "Employees",
+                column: "positionid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductDetails_ProductId1",
+                table: "ProductDetails",
+                column: "ProductId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Customers_CustomerId",
+                table: "Carts",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoides_Customers_CustomerId",
+                table: "Invoides",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoides_Employees_EmployeeId",
+                table: "Invoides",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Producers_ProducerId",
+                table: "Products",
+                column: "ProducerId",
+                principalTable: "Producers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Customers_CustomerId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoides_Customers_CustomerId",
+                table: "Invoides");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoides_Employees_EmployeeId",
+                table: "Invoides");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Producers_ProducerId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Producers");
+
+            migrationBuilder.DropTable(
+                name: "ProductDetails");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProducerId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoides_CustomerId",
+                table: "Invoides");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoides_EmployeeId",
+                table: "Invoides");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_CustomerId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "Createddate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProducerId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Createddate",
+                table: "Invoides");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Invoides");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Invoides");
+
+            migrationBuilder.DropColumn(
+                name: "Createddate",
+                table: "InvoiceDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "InvoiceDetails");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Carts");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Invoides",
+                newName: "id");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "InvoiceDetails",
+                newName: "id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "ProductTypes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AddColumn<string>(
+                name: "TypeName",
+                table: "ProductTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "Invoides",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Invoides",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Avatar = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fullname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsAdmin = table.Column<bool>(type: "bit", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoides_AccountId",
+                table: "Invoides",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_AccountId",
+                table: "Carts",
+                column: "AccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Accounts_AccountId",
+                table: "Carts",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoides_Accounts_AccountId",
+                table: "Invoides",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
